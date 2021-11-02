149d94f74c49eaae37695421605d89e1432f996d
Ivan <fridey0@gmail.com>
2021-11-02 11:15:01 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { debounceTime } from 'rxjs/operators';
 
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'github-search';
+export class AppComponent implements OnInit {
+  searchControl = new FormControl();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.searchControl.valueChanges.pipe(
+      untilDestroyed(this),
+      debounceTime(300)
+    ).subscribe({
+      next: value => {}
+    });
+  }
 }
